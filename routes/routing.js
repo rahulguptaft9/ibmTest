8b52332fabef6806ebc2879787b9c303d4661f87
aman giri <aman.giri0104@gmail.com>
2021-09-29 09:18:48 +0530

    file deleted for db setup.

--- routes/routing.js
+++ routes/routing.js
@@ -1,21 +1,7 @@
-var db = require("../utilities/dbSetup");
-
 var user = require("../service/user");
 
 var express = require("express");
 var router = express.Router();
-
-// router.get("/setupdb",(req,res)=>{
-//     db().then(result=>{
-//         console.log(result);
-//         if (result){
-//             res.json({"message": "insertion successfull"});
-//         }
-//         else{
-//             res.json({"message":"insertion failed."})
-//         }
-//     })
-// })
 
 
 router.post('/order-food', (req,res,next)=>{
